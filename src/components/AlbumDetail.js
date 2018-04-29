c3862f7d5687c3e702e454c31b9c45fd9196a0a7
Anirudh Jain <cynide@Shubhams-iMac.local>
2018-04-29 18:58:23 +0530

    App's main work has been completed

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './button'
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
 
   return (
     <Card>
+
       <CardSection>
         <View style={thumbnailContainerStyle}>
           <Image source={{ uri: thumbnail_image}} style={thumbnailStyle}/>
@@ -18,9 +20,17 @@ const AlbumDetail = ({ album }) => {
           <Text> {artist} </Text>
         </View>
       </CardSection>
+
       <CardSection>
         <Image source={{ uri: image }} style={imageStyle}/>
       </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
+      </CardSection>
+
     </Card>
   );
 };
